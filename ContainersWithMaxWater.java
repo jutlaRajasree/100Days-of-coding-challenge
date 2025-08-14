a31e079a7a58018008e12355e74f93c19b239941
jutlaRajasree <j.rajasree8121@gmail.com>
2025-08-14 15:47:33 +0530

    Add files via upload

---
+++ ContainersWithMaxWater.java
@@ -0,0 +1,35 @@
+package com.palle;
+
+public class ContainersWithMaxWater {
+	 public int maxArea(int[] height) {
+	        int left = 0;
+	        int right = height.length - 1;
+	        int maxArea = 0;
+
+	        while (left < right) {
+	            int width = right - left;
+	            int minHeight = Math.min(height[left], height[right]);
+	            int area = width * minHeight;
+	            maxArea = Math.max(maxArea, area);
+
+	            if (height[left] < height[right]) {
+	                left++;
+	            } else {
+	                right--;
+	            }
+	        }
+
+	        return maxArea;
+	    }
+
+	    public static void main(String[] args) {
+	        ContainersWithMaxWater solution = new ContainersWithMaxWater();
+	        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+	        int maxArea = solution.maxArea(height);
+	        System.out.println("Maximum area: " + maxArea);
+	    }
+	}
+
+
+
+
